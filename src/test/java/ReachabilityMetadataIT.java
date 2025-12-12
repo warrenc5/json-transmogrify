60a8686d9a36a66de708a2176cd9ea0de9917862
Warren Crossing <warren.crossing@netlinkz.com>
2025-12-12 16:16:02 +1100

    add ReachabilityMetaData Test

---
+++ src/test/java/ReachabilityMetadataIT.java
@@ -0,0 +1,35 @@
+
+import jakarta.json.Json;
+import jakarta.json.JsonPatch;
+import jakarta.json.JsonStructure;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.xml.transform.TransformerException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author wozza
+ */
+public class ReachabilityMetadataIT {
+
+    public static final String DATA = "/native-image/reachability-metadata.json";
+
+    @Test
+    public void testDiff() throws IOException, TransformerException {
+        JsonStructure orig = Json.createReader(ClassLoader.getSystemResourceAsStream("META-INF" + DATA)).readObject();
+        JsonStructure gen = Json.createReader(new FileReader("target" + DATA)).readObject();
+
+        JsonPatch diff = Json.createDiff(orig, gen);
+
+        assertNotNull(diff);
+        assertNotNull(diff.toJsonArray());
+        assertFalse(diff.toJsonArray().isEmpty());
+        assertNotNull(diff.toJsonArray().getFirst());
+        System.out.println(diff.toString());
+
+    }
+}
